69ce897ac94c9eb6bcacfd7e99b657ff8b04fb0c
Eca2107 <ecabralalonso@gmail.com>
2021-10-17 23:15:40 -0300

    entrega 5 pronta

--- js/cart2.js
+++ js/cart2.js
@@ -12,22 +12,31 @@ function insertProducts(array) {
       <div class="row w-100 border-bottom">
         <div class="row main align-items-center justify-content-between">
           <div class="col justify-content-center"><img class="img-fluid img-thumbnail" style="max-height: 110px;" src="` +
-          item.src +
-          `"></div>
+      item.src +
+      `"></div>
             <div class="col justify-content-center" style="text-align: center;">` +
-            item.name +
-            `</div>                      
+      item.name +
+      `</div>                      
             <div class="col align-items-center"><div class="input-group mb-3">
-              <button class="input-group-text" style="max-width: 40px;" onclick="this.nextElementSibling.stepDown()"><i class="bi bi-dash-lg"></i></button>
+              <button class="input-group-text" style="max-width: 40px;" id="` +
+      item.name +
+      `R" onclick="update(this.id);"><i class="bi bi-dash-lg"></i></button>
               <input type="number" style="text-align:center; max-width: 50px;" class="form-control" value="` +
-              item.count +
-              `" min="0" id="`+ item.name +`">
-              <button class="input-group-text" style="max-width: 40px;" onclick="this.previousElementSibling.stepUp()"><i class="bi bi-plus-lg"></i></button>
+      item.count +
+      `" min="0" id="` +
+      item.name +
+      `">
+              <button class="input-group-text" style="max-width: 40px;" id="` +
+      item.name +
+      `S" onclick="update(this.id);"><i class="bi bi-plus-lg"></i></button>
               </div></div>
             <div class="col">
               <div class="row" style="text-align: center;">Precio por unidad:<br>
-              <b>` + item.currency +`  `+ item.unitCost +
-              `</b></div>
+              <b>` +
+      item.currency +
+      `  ` +
+      item.unitCost +
+      `</b></div>
             </div>
         </div>
       </div>     
@@ -45,15 +54,57 @@ function insertProducts(array) {
       item.count +
       `</p> `;
 
-      valores += `<p class="text-muted" style="font-size: 15px; text-align: right;">`+ item.currency +` ` +
-      item.count*item.unitCost +
-      `</p> `
+    valores +=
+      `<p class="text-muted" style="font-size: 15px; text-align: right;">` +
+      item.currency +
+      ` ` +
+      item.count * item.unitCost +
+      `</p> `;
   }
 
   document.getElementById("itemsCarrito").innerHTML = htmlContentToAppend;
   document.getElementById("items").innerHTML = htmlContentToAppend2;
   document.getElementById("cant").innerHTML = cant;
   document.getElementById("subtotal").innerHTML = valores;
+}
+
+function update(id) {
+  let valor1 = document.getElementById(
+    "Pino de olor para el auto"
+  ).valueAsNumber;
+  let valor2 = document.getElementById("Suzuki Celerio").valueAsNumber;
+
+  switch (id) {
+    case "Pino de olor para el autoS":
+      document.getElementById("Pino de olor para el auto").valueAsNumber++;
+
+      break;
+
+    case "Pino de olor para el autoR":
+      if (valor1 > 0) {
+        document.getElementById("Pino de olor para el auto").valueAsNumber--;
+      } else {
+        alert("a donde vas?");
+      }
+      break;
+
+    case "Suzuki CelerioS":
+      document.getElementById("Suzuki Celerio").valueAsNumber++;
+
+      break;
+
+    case "Suzuki CelerioR":
+      if (valor2 > 0) {
+        document.getElementById("Suzuki Celerio").valueAsNumber--;
+      } else {
+        alert("a donde vas?");
+      }
+      break;
+
+    default:
+      alert("nel");
+      break;
+  }
 }
 
 function tipoDeEnvio() {
@@ -89,5 +140,5 @@ document.addEventListener("DOMContentLoaded", function (e) {
       //Muestro los items cargados del JSON
       insertProducts(itemsArray.articles);
     }
-  });  
+  });
 });
